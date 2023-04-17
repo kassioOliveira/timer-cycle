27befde7ee9a870a0d90f97f3a57dc7b2a2f57ef
kassio oliveira <kassiooliveira875@gmail.com>
2023-04-17 17:15:55 -0300

    feat: add theme to global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "styled-components"
+
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
 
 export function App() {
 
   return (
-    <div className="App">
-     ola
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
