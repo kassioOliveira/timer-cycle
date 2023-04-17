15d13ce6342573044c7dc61e9979b26a44dace25
kassio oliveira <kassiooliveira875@gmail.com>
2023-04-17 17:45:00 -0300

    styled: fixing eslint problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { ThemeProvider } from "styled-components"
+import { ThemeProvider } from 'styled-components'
 
-import { GlobalStyle } from "./styles/global"
-import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
     </ThemeProvider>
   )
 }
-
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
  * {
@@ -9,13 +9,13 @@ export const GlobalStyle = createGlobalStyle`
 
 :focus{
     outline: 0;
-    box-shadow:0 0 0 2px ${(props) => props.theme["green-500"]};
+    box-shadow:0 0 0 2px ${(props) => props.theme['green-500']};
 }
 
 
 body{
-    background: ${(props) => props.theme["gray-900"]};
-    color: ${(props) => props.theme["gray-300"]};
+    background: ${(props) => props.theme['gray-900']};
+    color: ${(props) => props.theme['gray-300']};
 }
 
 body,input-security, textarea, button{
@@ -24,4 +24,4 @@ body,input-security, textarea, button{
     font-size:1rem;
 }
 
-`;
+`
