eaf6cd84c9fb8494990f1c036db9321936a84054
kassio oliveira <kassiooliveira875@gmail.com>
2023-04-18 14:48:29 -0300

    feat: header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { HeaderContainer } from './style'
+import { Timer, Scroll } from 'phosphor-react'
+
+import logo from '../../assets/Logo.svg'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logo} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
